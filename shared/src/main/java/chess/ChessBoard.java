0574ff661202e357e6931bea28eef764dbc85b2d
Kent Lau <kent.lau1248@gmail.com>
2026-09-16 00:26:47 -0600

    Implemented everything but ChessGame and ChessPiece for basic movement

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -40,6 +40,6 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        squares = new ChessPiece[8][8];
     }
 }
